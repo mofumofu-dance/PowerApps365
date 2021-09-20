5aa4c280210c4a3b39335090659a0b883dbfdbb8
mofumofu-dance <59677945+mofumofu-dance@users.noreply.github.com>
2021-09-20 16:29:32 +0900

    Create CustomCode.cs

---
+++ Samples/CodeConnector/CustomCode.cs
@@ -0,0 +1,13 @@
+{
+    public override async Task<HttpResponseMessage> ExecuteAsync()
+    {
+        var contentAsString = await this.Context.Request.Content.ReadAsStringAsync().ConfigureAwait(false);
+        var stringJArray = JArray.Parse(contentAsString);
+        var floatArray = stringJArray.ToObject<List<float>>();
+        float ave = floatArray.Average();
+        float sum = floatArray.Sum();
+        HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+        response.Content = CreateJsonContent("{\"average\": "+ave+",\"sum\":"+sum+"}");
+        return response;
+    }
+}
