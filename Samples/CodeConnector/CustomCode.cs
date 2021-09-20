ad95d1af74a44525aed7eac09dc2ee3ff880db03
mofumofu-dance <59677945+mofumofu-dance@users.noreply.github.com>
2021-09-20 16:29:59 +0900

    Update CustomCode.cs

--- Samples/CodeConnector/CustomCode.cs
+++ Samples/CodeConnector/CustomCode.cs
@@ -1,11 +1,12 @@
+public class Script : ScriptBase
 {
     public override async Task<HttpResponseMessage> ExecuteAsync()
     {
         var contentAsString = await this.Context.Request.Content.ReadAsStringAsync().ConfigureAwait(false);
         var stringJArray = JArray.Parse(contentAsString);
-        var floatArray = stringJArray.ToObject<List<float>>();
-        float ave = floatArray.Average();
-        float sum = floatArray.Sum();
+        var doubleArray = stringJArray.ToObject<List<float>>();
+        float ave = doubleArray.Average();
+        float sum = doubleArray.Sum();
         HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
         response.Content = CreateJsonContent("{\"average\": "+ave+",\"sum\":"+sum+"}");
         return response;
